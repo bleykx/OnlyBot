b98a1d314804eadd52bbcb482dcef08e97ae52a8
unknown <pi2r34@gmail.com>
2024-07-09 13:01:02 +0200

    Yessai

---
+++ OnlyBot_Client/Helpers/SearchHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+using OnlyBot_Client.Shared.OnlyBotComponents.Menus;
+
+namespace OnlyBot_Client.Helpers
+{
+    public static class SearchHelper
+    {
+        [JSInvokable]
+        public static void UpdateSearchQuery(string query)
+        {
+            _ScriptMenu.Instance.UpdateSearchQuery(query);
+        }
+    }
+}
